cfbf213cc3fbbf9075146313873ce0615ef4e419
Vynicius Pontes <pontes.vynicius@gmail.com>
2018-07-11 22:42:07 -0300

    Adding repositories to StudentService

--- internships/src/main/java/br/com/uff/internships/service/StudentService.java
+++ internships/src/main/java/br/com/uff/internships/service/StudentService.java
@@ -1,24 +1,33 @@
 package br.com.uff.internships.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.uff.internships.entity.StudentSkill;
+import br.com.uff.internships.entity.User;
 import br.com.uff.internships.form.RegistrationStudentForm;
-import br.com.uff.internships.repository.ForeignLanguageRepository;
+import br.com.uff.internships.repository.StudentForeignLanguageRepository;
 import br.com.uff.internships.repository.StudentRepository;
 import br.com.uff.internships.repository.UserRepository;
 
 @Service
 public class StudentService {
 	
+	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
 	private StudentRepository studentRepository;
 	
-	private ForeignLanguageRepository foreignLanguageRepository;
+	@Autowired
+	private StudentForeignLanguageRepository studentForeignLanguageRepository;
+	
+	@Autowired
+	private StudentSkill studentSkill;
 	
 	public void saveNewStudent(RegistrationStudentForm form) {
 		
-		
+		User newUser = new User();
 		
 	}
 	
